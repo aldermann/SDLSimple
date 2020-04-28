f367828ab3f3c75dbe566f6dd95c659f42db8f2d
aldermann <trancongvietan22@gmail.com>
2020-04-28 09:49:37 +0700

    Fix stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,3 @@
-/*
- * Copyright (c) 2018, 2019 Amine Ben Hassouna <amine.benhassouna@gmail.com>
- * All rights reserved.
- *
- * Permission is hereby granted, free of charge, to any
- * person obtaining a copy of this software and associated
- * documentation files (the "Software"), to deal in the
- * Software without restriction, including without
- * limitation the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software
- * is furnished to do so, subject to the following
- * conditions:
- *
- * The above copyright notice and this permission notice
- * shall be included in all copies or substantial portions
- * of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
- * ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
- * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
- * SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
- * IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- *
- */
-
 #include <iostream>
 #include <algorithm>
 
@@ -36,15 +6,13 @@
 #define SCREEN_WIDTH    800
 #define SCREEN_HEIGHT   600
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
     // Unused argc, argv
     (void) argc;
     (void) argv;
 
     // Initialize SDL
-    if(SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cout << "SDL could not be initialized!" << std::endl
                   << "SDL_Error: " << SDL_GetError() << std::endl;
         return 0;
@@ -65,22 +33,16 @@ int main(int argc, char* argv[])
                                           SDL_WINDOWPOS_UNDEFINED,
                                           SCREEN_WIDTH, SCREEN_HEIGHT,
                                           SDL_WINDOW_SHOWN);
-    if(!window)
-    {
+    if (!window) {
         std::cout << "Window could not be created!" << std::endl
                   << "SDL_Error: " << SDL_GetError() << std::endl;
-    }
-    else
-    {
+    } else {
         // Create renderer
         SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-        if(!renderer)
-        {
+        if (!renderer) {
             std::cout << "Renderer could not be created!" << std::endl
                       << "SDL_Error: " << SDL_GetError() << std::endl;
-        }
-        else
-        {
+        } else {
             // Declare rect of square
             SDL_Rect squareRect;
 
@@ -97,16 +59,14 @@ int main(int argc, char* argv[])
             bool quit = false;
 
             // Event loop
-            while(!quit)
-            {
+            while (!quit) {
                 SDL_Event e;
 
                 // Wait indefinitely for the next available event
                 SDL_WaitEvent(&e);
 
                 // User requests quit
-                if(e.type == SDL_QUIT)
-                {
+                if (e.type == SDL_QUIT) {
                     quit = true;
                 }
 
